be0048ac5fdb56ae0c7a87bda3e81b4d2ed2d3d6
Rafael Tutui Delhaye <rafael.rtd@pf.gov.br>
2020-04-23 12:32:53 -0400

    Removido o comentario do fonte

--- teste de comandos.js
+++ teste de comandos.js
@@ -1,13 +1,4 @@
-/*n01, n02: real
 
-Inicio
-// Seção de Comandos, procedimento, funções, operadores, etc... 
-n01:=10
-n02:=15
-se (n01 <> n02) entao
-   escreval ("n01 é diferente")
-senao
-        escreval("n01 é igual.")*/
         var n01, n02;
 
         n01 = prompt("Digite o primeiro numero")
